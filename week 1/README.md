bd09e7f8819aca33f494a6325b0fe9a11584a302
pikachuvvaishnavi <vaishnavi.code.127@gmail.com>
2026-05-16 00:35:01 +0530

    week 1 1st commit

---
+++ week 1/README.md
@@ -0,0 +1,11 @@
+# Personal Introduction Program
+
+This is a beginner Python project created for Week 1 internship tasks.
+
+## Features
+- Takes user input
+- Stores data in variables
+- Displays a welcome message
+
+## Technologies Used
+- Python
---
+++ week 1/personal_intro.py
@@ -0,0 +1,49 @@
+# print("Hello World!") #Day 1 of The Developers Arena Data Science Course
+
+# name = "Vaishnavi" #Day 2 variables and input
+# age = 23
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+
+
+# print(f"My name is {name}") #Day 3 String formatting and f-strings
+# print(f"My age is {age}")
+
+# Personal Introduction Program
+# Week 1 Internship Task
+
+# Personal Introduction Program
+# Week 1 Internship Task
+
+print("===================================")
+print("   Welcome to My Introduction App")   #Title of the program
+print("===================================\n")
+
+# Taking user input
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+city = input("Which city do you live in? ")
+college = input("Which college did you study in? ")
+course = input("What course did you study? ")
+hobby = input("What is your favorite hobby? ")
+favorite_food = input("What is your favorite food? ")
+dream_job = input("What is your dream job? ")
+favorite_color = input("What is your favorite color? ")
+pet = input("Do you have a pet? If yes, what is it? ")
+
+# Displaying output
+print("\n===================================")
+print("          Your Introduction")
+print("===================================\n")
+
+print(f"Hello {name}! ")
+print(f"You are {age} years old and live in {city}.")
+print(f"You studied {course} at {college}. ")
+print(f"Your favorite hobby is {hobby}. ")
+print(f"You love eating {favorite_food}. ")
+print(f"Your dream job is to become a {dream_job}. ")
+print(f"Your favorite color is {favorite_color}. ")
+print(f"Your pet information: {pet}. 🐾")
+
+print("\n✨ Thank you for sharing your details! ✨")
+print("Have a wonderful day! ")
